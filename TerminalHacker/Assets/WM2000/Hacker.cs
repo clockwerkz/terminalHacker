5cf8b87031ca8f51a36397f221eb5999d6f2087f
clockwerkz <clockwerkz@gmail.com>
2020-10-17 00:24:23 -0700

    adds a function to print the main menu

--- TerminalHacker/Assets/WM2000/Hacker.cs
+++ TerminalHacker/Assets/WM2000/Hacker.cs
@@ -7,12 +7,24 @@ public class Hacker : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hello console!");
+        showMainMenu();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void showMainMenu()
+    {
+        Terminal.WriteLine("What would you like to hack into? \n");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Press 3 for NASA");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter your selection:");
     }
 }
